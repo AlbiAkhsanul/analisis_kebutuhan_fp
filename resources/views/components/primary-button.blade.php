3ff1f3c80cee13a1b60f51bea84e53fdf586bd6a
mikhail-karim <mikhailsakarim@gmail.com>
2025-05-13 06:29:08 +0700

    tampilan login

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest w-52 h-14 left-0 top-0 absolute bg-red-700 rounded-[12px] shadow-[2px_2px_2px_1px_rgba(0,0,0,0.25)] absolute text-center justify-center transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
