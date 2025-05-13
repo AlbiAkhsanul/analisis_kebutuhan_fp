6d9126345b11a14e47d4d851d3fbbdb24d3228eb
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2025-05-13 21:52:43 +0700

    adding user seeders and testing views

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,7 +16,10 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::with('partner')->get();
-        return view('projects.index', compact('projects'));
+        $user = auth()->user();
+
+        // return view('projects.index', compact('projects', 'user'));
+        return view('home(farel)', compact('projects', 'user')); //Coba memanggil home punya mas farel
     }
 
     /**
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'nama_pengguna' => 'Admin',
+                'password' => Hash::make('password123'),
+                'email_pengguna' => 'admin@example.com',
+                'jobdesk' => 'Administrator',
+                'is_admin' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'deleted_at' => null,
+            ],
+            [
+                'nama_pengguna' => 'Regular',
+                'password' => Hash::make('userpassword'),
+                'email_pengguna' => 'user@example.com',
+                'jobdesk' => 'Stakeholder',
+                'is_admin' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'deleted_at' => null,
+            ]
+        ]);
+    }
+}
