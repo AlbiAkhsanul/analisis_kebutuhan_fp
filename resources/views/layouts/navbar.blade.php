c8496bcbe7b2918873ff18170858094d158a8c2a
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2025-05-18 12:50:48 +0700

    navbar update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,8 +5,30 @@
     </div>
 
     <div>
-        <p class="text-black fw-bold px-4 py-2">
-            {{ \Carbon\Carbon::now()->translatedFormat('l, d F Y | H:i:s') }} WIB
-        </p>
+        <div>
+            <p id="current-time" class="text-black fw-bold px-4 py-2"></p>
+        </div>
     </div>   
 </header>
+<script>
+    function updateTime() {
+        const now = new Date();
+
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false,
+            // timeZone: 'Asia/Jakarta'
+        };
+
+        const formattedTime = new Intl.DateTimeFormat('id-ID', options).format(now) + ' WIB';
+        document.getElementById('current-time').textContent = formattedTime;
+    }
+
+    updateTime(); // initial
+    setInterval(updateTime, 60000); // update setiap menit
+</script>
