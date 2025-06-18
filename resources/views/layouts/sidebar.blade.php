3dd55a98462e6af9e0636c5915bed8582c094673
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2025-06-19 00:38:55 +0700

    added default profile image

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="col-lg-2 mt-4 text-white p-2 border-end" style="min-height: 80vh;">
     <div class="text-start ms-3 mb-3 mt-3">
-        <img src="{{ asset('src/anomali.jpeg') }}" alt="User" class="rounded-circle" width="100" style="border-radius: 121px; border: 5px white solid" src="https://placehold.co/100x100">
+        <img src="{{ asset('src/defaultPfp.jpg') }}" alt="User" class="rounded-circle" width="100" style="border-radius: 121px; border: 5px white solid" src="https://placehold.co/100x100">
     </div>
     <table class="text-white table-borderless mb-0 ms-3" style="background-color: transparent;">
         <tbody>
